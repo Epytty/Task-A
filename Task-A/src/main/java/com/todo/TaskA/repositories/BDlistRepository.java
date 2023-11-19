4c9c54f6f1df4f0e5fd920ad780051bdb2af4265
Daniil <84202298+Epytty@users.noreply.github.com>
2023-11-19 21:01:35 +0300

    Delete Task-A/src directory

--- Task-A/src/main/java/com/todo/TaskA/repositories/BDlistRepository.java
+++
@@ -1,8 +0,0 @@
-package com.todo.TaskA.repositories;
-
-import com.todo.TaskA.models.BDlist;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BDlistRepository extends CrudRepository<BDlist, Long> {
-
-}
