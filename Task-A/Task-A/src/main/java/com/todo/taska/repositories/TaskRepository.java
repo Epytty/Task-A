46a8f4f2ab3f9bd61bd0d56c529a70eb2d738724
Daniil <84202298+Epytty@users.noreply.github.com>
2023-11-23 18:22:17 +0300

    Delete Task-A/Task-A directory

--- Task-A/Task-A/src/main/java/com/todo/taska/repositories/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.todo.taska.repositories;
-
-import com.todo.taska.models.TaskEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskRepository extends CrudRepository<TaskEntity, Long> {
-
-}
